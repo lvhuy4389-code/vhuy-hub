ab228858ce2012e630b850edd516d23ff69abcc9
lvhuy4389-code <lvhuy4389@gmail.com>
2026-09-10 18:15:21 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,1 @@
--- VHUY HUB (Steal An Egg)
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bf52181bce5c28e6ecd16eebfa1a88ddaf646c7acb10015fbaa7b30dc7bad0a8/download"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vhuyxscripts/vhuyhubv2/main/stealaneggv2"))(
